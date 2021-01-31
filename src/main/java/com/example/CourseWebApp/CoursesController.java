d36f2e940862bdb6c3ac1e4e8884c99084e3da0b
Gam <trant48@student.douglascollege.ca>
2021-01-31 13:07:08 -0800

    initial commit

---
+++ src/main/java/com/example/CourseWebApp/CoursesController.java
@@ -0,0 +1,11 @@
+package com.example.CourseWebApp;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class CoursesController {
+	public void courses() {
+		System.out.println("Hi Gam!");
+	}
+
+}
